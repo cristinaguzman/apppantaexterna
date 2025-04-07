80961f168b9d9847bd32d4d8f1df4a9fdb580f64
Criss <cristinaguzman@live.cl>
2025-04-07 17:56:55 -0400

    cambio de footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Footer = () => {
   return (
     <footer className="bg-primary text-white text-center py-2 mt-auto">
-      <small style={{ fontSize: "0.9 rem" }}>© 2025 Task Flow - Todos los derechos reservados</small>
+      <small style={{ fontSize: "0.9 rem" }}>© 2025 Compañia Santy - Todos los derechos reservados</small>
     </footer>
   );
 };
